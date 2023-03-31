728000b01a825e89242777d3eec439a1cd9a23e2
TLRKiliann <philogenie@protonmail.com>
2023-03-31 22:03:07 +0200

    script improved with supervariable, session & cookie

--- php/app/components/header.php
+++ php/app/components/header.php
@@ -4,15 +4,15 @@ $style;
 $favicon;
 ?>
 
-<meta charset='UTF-8' />
-<meta name="keywords" content="HTML, CSS, PhP">
-<meta name="description" content="Free Web tutorials">
-<meta name="author" content="John Doe">
-<meta http-equiv="refresh" content="30">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" type="text/css" href="<?php echo $style; ?>" />
-<link rel="icon" type="image/png" href="<?php echo $favicon; ?>">
-<title><?php echo $title; ?></title>
+	<meta charset='UTF-8' />
+	<meta name="keywords" content="HTML, CSS, PhP">
+	<meta name="description" content="Free Web tutorials">
+	<meta name="author" content="John Doe">
+	<meta http-equiv="refresh" content="30">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="<?php echo $style; ?>" />
+	<link rel="icon" type="image/png" href="<?php echo $favicon; ?>">
+	<title><?php echo $title; ?></title>
 
 
 
